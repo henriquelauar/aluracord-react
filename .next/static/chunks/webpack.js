e514e27f2eb97cceee8dba4b49b11897dd462974
henrique lauar <henriquelauar2002@gmail.com>
2022-02-01 02:54:01 -0300

    Adding preview images to use in the readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d6feefc7ea97aeb4"; }
+/******/ 		__webpack_require__.h = function() { return "b3ec14af7111b169"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
